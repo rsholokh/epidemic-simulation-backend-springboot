b8a9102b1e0e21a4b718a1f6112dc6d592947bf9
Roman <sholokh.r@gmail.com>
2021-06-01 23:02:05 +0200

    Added "add" functionality for UserData in UserDataService & UserDataController

--- src/main/java/com/romansholokh/epidemicsimulation/backendspringboot/controller/UserDataController.java
+++ src/main/java/com/romansholokh/epidemicsimulation/backendspringboot/controller/UserDataController.java
@@ -1,12 +1,28 @@
 package com.romansholokh.epidemicsimulation.backendspringboot.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.romansholokh.epidemicsimulation.backendspringboot.entity.UserData;
+import com.romansholokh.epidemicsimulation.backendspringboot.service.UserDataService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/user-data")
 @CrossOrigin(origins = "http://localhost:4200")
 public class UserDataController {
 
+    private final UserDataService userDataService;
+
+    @PostMapping("/add")
+    public ResponseEntity<UserData> add(@RequestBody UserData userData) {
+        if (userData.getId() != 0) {
+            return new ResponseEntity("redundant param: id MUST NOT exist or MUST be 0", HttpStatus.NOT_ACCEPTABLE);
+        } else if (userData.getSimulationName() == null || userData.getSimulationName().trim().length() == 0) {
+            return new ResponseEntity("missed param: simulationName", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(userDataService.add(userData));
+    }
 }
--- src/main/java/com/romansholokh/epidemicsimulation/backendspringboot/service/UserDataService.java
+++ src/main/java/com/romansholokh/epidemicsimulation/backendspringboot/service/UserDataService.java
@@ -1,9 +1,20 @@
 package com.romansholokh.epidemicsimulation.backendspringboot.service;
 
+import com.romansholokh.epidemicsimulation.backendspringboot.entity.UserData;
+import com.romansholokh.epidemicsimulation.backendspringboot.repo.UserDataRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
+@AllArgsConstructor
 public class UserDataService {
+
+    private final UserDataRepository userDataRepository;
+
+    public UserData add(UserData userData)
+    {
+        return userDataRepository.save(userData);
+    }
 }
