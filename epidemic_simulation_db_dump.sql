6a8f887d49b4f766b3a3c454e36651bb8467233a
Roman <sholokh.r@gmail.com>
2021-06-01 20:33:21 +0200

    Attach MySQL database dump

---
+++ epidemic_simulation_db_dump.sql
@@ -0,0 +1,89 @@
+CREATE DATABASE  IF NOT EXISTS `epidemic_simulation_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `epidemic_simulation_db`;
+-- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
+--
+-- Host: localhost    Database: epidemic_simulation_db
+-- ------------------------------------------------------
+-- Server version	8.0.25
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `genereted_values`
+--
+
+DROP TABLE IF EXISTS `genereted_values`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `genereted_values` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `day` bigint NOT NULL,
+  `number_of_infected` bigint NOT NULL,
+  `number_of_healthy_susceptible_to_infection` bigint NOT NULL,
+  `death_toll` bigint NOT NULL,
+  `number_of_recovered_and_acquired_immunity` bigint NOT NULL,
+  `user_data_id` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_data_id_fk_idx` (`user_data_id`),
+  CONSTRAINT `user_data_id_fk` FOREIGN KEY (`user_data_id`) REFERENCES `user_data` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `genereted_values`
+--
+
+LOCK TABLES `genereted_values` WRITE;
+/*!40000 ALTER TABLE `genereted_values` DISABLE KEYS */;
+/*!40000 ALTER TABLE `genereted_values` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_data`
+--
+
+DROP TABLE IF EXISTS `user_data`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_data` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `simulation_name` varchar(100) NOT NULL,
+  `population_size` bigint NOT NULL,
+  `initial_number_of_infected` bigint NOT NULL,
+  `infectiousness` bigint NOT NULL,
+  `mortality` bigint NOT NULL,
+  `days_until_recovery` bigint NOT NULL,
+  `days_until_death` bigint NOT NULL,
+  `simulation_days` bigint NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_data`
+--
+
+LOCK TABLES `user_data` WRITE;
+/*!40000 ALTER TABLE `user_data` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_data` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-06-01 19:39:16
