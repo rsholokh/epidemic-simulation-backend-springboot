1a0d99caafc45ee7ba0aeeb41884b666c0de070d
Roman <sholokh.r@gmail.com>
2021-06-01 20:30:21 +0200

    Initial commit

---
+++ src/main/java/com/romansholokh/epidemicsimulation/backendspringboot/EpidemicSimulationBackendSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.romansholokh.epidemicsimulation.backendspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpidemicSimulationBackendSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpidemicSimulationBackendSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/romansholokh/epidemicsimulation/backendspringboot/EpidemicSimulationBackendSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romansholokh.epidemicsimulation.backendspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpidemicSimulationBackendSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
