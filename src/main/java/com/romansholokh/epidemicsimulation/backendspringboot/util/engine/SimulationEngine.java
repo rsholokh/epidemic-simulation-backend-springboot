70fdc67b73a38b49fc014803877e7bdf9e783baa
Roman <sholokh.r@gmail.com>
2021-06-03 20:28:02 +0200

    Created SimulationEngine & SimulationEngineImpl. Added basic functionality

--- src/main/java/com/romansholokh/epidemicsimulation/backendspringboot/util/engine/SimulationEngine.java
+++ src/main/java/com/romansholokh/epidemicsimulation/backendspringboot/util/engine/SimulationEngine.java
@@ -1,4 +1,8 @@
 package com.romansholokh.epidemicsimulation.backendspringboot.util.engine;
 
+import com.romansholokh.epidemicsimulation.backendspringboot.entity.UserData;
+
 public interface SimulationEngine {
+
+    public void simulating(UserData userData);
 }
--- src/main/java/com/romansholokh/epidemicsimulation/backendspringboot/util/engine/SimulationEngineImpl.java
+++ src/main/java/com/romansholokh/epidemicsimulation/backendspringboot/util/engine/SimulationEngineImpl.java
@@ -1,7 +1,57 @@
 package com.romansholokh.epidemicsimulation.backendspringboot.util.engine;
 
-import com.romansholokh.epidemicsimulation.backendspringboot.util.engine.SimulationEngine;
+import com.romansholokh.epidemicsimulation.backendspringboot.controller.GeneratedValuesController;
+import com.romansholokh.epidemicsimulation.backendspringboot.entity.GeneratedValues;
+import com.romansholokh.epidemicsimulation.backendspringboot.entity.UserData;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
 
+@Component
+@AllArgsConstructor
 public class SimulationEngineImpl implements SimulationEngine {
 
+    private final GeneratedValuesController generatedValuesController;
+    private GeneratedValues generatedValues;
+
+    @Override
+    public void simulating(UserData userData) {
+
+        long i = userData.getInitialNumberOfInfected();
+        long r = userData.getInfectiousness();
+        long m = userData.getMortality();
+        long tm = userData.getDaysUntilDeath();
+        long ti = userData.getDaysUntilRecovery();
+
+
+
+        long presentDay = 0;
+
+        long pi;
+        long pv;
+        long pm;
+        long pr;
+
+//        while(presentDay <= userData.getSimulationDays()) {
+            if (presentDay == 0) {
+                pi = i;
+                pv = i * r;
+                pm = 0;
+                pr = 0;
+                this.sendToDB(pi, pv, pm, pr, presentDay, userData);
+                presentDay++;
+                System.out.println("success simulating!");
+            }
+//        }
+    }
+
+    private void sendToDB(long pi, long pv, long pm, long pr, long presentDay, UserData userData) {
+        generatedValues.setNumberOfInfected(pi);
+        generatedValues.setNumberOfHealthySusceptibleToInfection(pv);
+        generatedValues.setDeathToll(pm);
+        generatedValues.setNumberOfRecoveredAndAcquiredImmunity(pr);
+        generatedValues.setDay(presentDay);
+        System.out.println("success sendToDB!");
+        generatedValues.setUserData(userData);
+        generatedValuesController.add(generatedValues);
+    }
 }
