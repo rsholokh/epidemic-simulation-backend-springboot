2a744d1870e8e67952027977710b8a278b51f634
Roman <sholokh.r@gmail.com>
2021-06-07 18:49:26 +0200

    Added GeneratedValues "getById" functionality to Controller & Service

--- src/main/java/com/romansholokh/epidemicsimulation/backendspringboot/controller/GeneratedValuesController.java
+++ src/main/java/com/romansholokh/epidemicsimulation/backendspringboot/controller/GeneratedValuesController.java
@@ -3,11 +3,12 @@ package com.romansholokh.epidemicsimulation.backendspringboot.controller;
 import com.romansholokh.epidemicsimulation.backendspringboot.entity.GeneratedValues;
 import com.romansholokh.epidemicsimulation.backendspringboot.service.GeneratedValuesService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
@@ -25,5 +26,17 @@ public class GeneratedValuesController {
         generatedValuesService.addAll(generatedValuesList);
     }
 
+    @GetMapping("/id/{id}")
+    public ResponseEntity<GeneratedValues> getById(@PathVariable Long id) {
+        GeneratedValues generatedValues = null;
+        Optional<GeneratedValues> optional = generatedValuesService.getById(id);
+        if (optional.isPresent()) {
+            generatedValues = optional.get();
+        } else {
+            return new ResponseEntity("id = " + id + " not found", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(generatedValues);
+    }
 
 }
--- src/main/java/com/romansholokh/epidemicsimulation/backendspringboot/service/GeneratedValuesService.java
+++ src/main/java/com/romansholokh/epidemicsimulation/backendspringboot/service/GeneratedValuesService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -21,5 +22,9 @@ public class GeneratedValuesService {
 
     public void addAll(List<GeneratedValues> generatedValuesList) {
         generatedValuesRepository.saveAll(generatedValuesList);
+    }
+
+    public Optional<GeneratedValues> getById(Long id) {
+        return generatedValuesRepository.findById(id);
     }
 }
